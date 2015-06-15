da9fff07c375d44975ac31a135ef44add33d4f8b
marko.budiselic <marko.budiselic@fer.hr>
2015-06-15 19:44:07 +0200

    grid search executor update

--- executors/grid_search/executor.py
+++ executors/grid_search/executor.py
@@ -6,10 +6,10 @@
 
 import logging
 
-from common.best_store import BestStore
-from common.initializer import problem_init, algorithm_init
-from helpers.grid_search import grid_item_container
+import numpy as np
 from helpers.path import unique_path
+from helpers.grid_search import grid_item_container
+from common.initializer import problem_init, algorithm_init
 
 log = logging.getLogger(__name__)
 
@@ -20,31 +20,46 @@ def execute(algorithms, problems, config):
     # init
     grid = grid_item_container(config.grid)
     context = config.common
-    best_store = BestStore()
 
     # execution
+    all_results = {}
     for element in grid:
-        context.hard_merge(element)
+        for it in range(int(config.repeat)):
+            all_results.setdefault(element, [])
+            context.hard_merge(element)
 
-        # evaluator configuration
-        (problem_config, problem_operator) = problem_init(problems, context)
+            # evaluator configuration
+            (problem_config, problem_operator) = problem_init(problems,
+                                                              context)
 
-        # algorithm configuration
-        (algorithm_config, algorithm) = algorithm_init(algorithms, context,
-                                                       problem_config,
-                                                       problem_operator)
+            # algorithm configuration
+            (algorithm_config, algorithm) = algorithm_init(algorithms, context,
+                                                           problem_config,
+                                                           problem_operator)
 
-        # algorithm execution
-        (solution, fitness) = algorithm.engine.run(algorithm_config)
+            # algorithm execution
+            solution = algorithm.engine.run(algorithm_config)
 
-        # save result
-        best_store.try_store(fitness, solution, element)
+            all_results[element].append(solution)
 
-    # prepare output
-    context.hard_merge(best_store.best_config)
-    output_path = unique_path('output', 'grid')
+    # calculate median values
+    for key in all_results:
+        all_results[key] = \
+            np.median(map(lambda x: x.fitness.value, all_results[key]))
 
-    # write output
-    with open(output_path, 'w') as f:
-        f.write(str(context))
-        f.write('Fitness: %s\n' % str(best_store.best_fitness))
+    # store best parameters
+    params, median = min(all_results.items(), key=lambda x: x[1])
+    output_max_path = unique_path('output', 'grid_search-%s-%s' %
+                                  (context.algorithm, context.problem))
+    params.store(output_max_path)
+    print params, median
+
+    # store heat map
+    output_heatmap_path = unique_path('output', 'heatmap-%s-%s' %
+                                      (context.algorithm, context.problem))
+    with open(output_heatmap_path, 'w') as f:
+        for params, median in all_results.items():
+            for param in sorted(params.keys()):
+                f.write('%s, ' % str(params[param]))
+            f.write('%s\n' % str(median))
+    print all_results
